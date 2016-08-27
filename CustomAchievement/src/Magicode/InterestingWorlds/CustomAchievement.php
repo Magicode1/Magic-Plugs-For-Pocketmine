8e8e234ba49daadf3049e09157e25c9eb7b3e277
Magicode <herbmcsherb@gmail.com>
2016-08-27 17:40:18 -0400

    Custom Acheivement Messages
    Still in progress!

---
+++ CustomAchievement/src/Magicode/InterestingWorlds/CustomAchievement.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Magicode\CustomAchievement;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerAchievementAwardedEvent;
+class CustomAchievement extends PluginBase{
+    
+    public function enEnable(){
+        $this->getLogger()->info("CustomAchievement loading.....");
+        $this->saveDefaultConfig();
+        $this->reloadConfig();
+    }
+        
+    public function onDisable(){
+        $this->getlogger()->info("CustomAchievement Disabling.....");
+}
+ public function getAcheivement(){
+     
+     
+     
+     
+     
+ }
+
+
+}
