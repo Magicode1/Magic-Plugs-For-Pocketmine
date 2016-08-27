5452b7b0421d43255ca37e05d841ef9d46546ead
Magicode <herbmcsherb@gmail.com>
2016-08-27 16:12:59 -0400

    Added InterestingWorld Plugin
    Not finished plugin. Just started. Plugin to be used for generating ores
    and caves in PM

---
+++ InterestingWorld/src/Magicode/InterestingWorld/InterestingWorld.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Magicode\NoMoreLag;
+
+use pocketmine\plugin\PluginBase;
+
+class NoMoreLag extends PluginBase{
+    public function onEnable() {
+       $this->getLogger()->info("NoMoreLag Enabling");
+    }
+    
+}
