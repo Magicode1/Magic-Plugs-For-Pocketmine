b550043d4e40172ff37a989b4f2c4231b2a1526d
Magicode <herbmcsherb@gmail.com>
2016-08-29 18:45:17 -0400

    Update UniquePlayers.php

--- UniquePlayers/src/Magicode/UniquePlayers/UniquePlayers.php
+++ UniquePlayers/src/Magicode/UniquePlayers/UniquePlayers.php
@@ -6,7 +6,13 @@ use pocketmine\plugin\PluginBase;
 
 class NoMoreLag extends PluginBase{
     public function onEnable() {
-       $this->getLogger()->info("NoMoreLag Enabling");
+       $this->getLogger()->info("UniquePlayers Enabling.....");
     }
-    
+        
+    public function onDisable(){
+        $this->getlogger()->info("UniquePlayers Disabling.....");
+}
+
+
+
 }
